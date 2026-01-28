db091d8d2d42e74e380b4fd3f8926c7efe7498d3
fromcode <wahyudeny780@gmail.com>
2026-01-28 22:13:43 +0700

    create main func for listener

--- main.go
+++ main.go
@@ -1,1 +1,28 @@
 package main
+
+import (
+	"log"
+	"net"
+)
+
+func main() {
+	s := newServer()
+	go s.run()
+
+	listener, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Fatalf("can't start the server: %s", err.Error())
+	}
+
+	defer listener.Close()
+	log.Printf("started server on :8080")
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Printf("can't accept this connection: %s", err.Error())
+			continue
+		}
+		go s.newClient(conn)
+	}
+}
