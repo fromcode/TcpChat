f864bc74a7dd8f7b7065dccde38d1a075e111fc1
fromcode <wahyudeny780@gmail.com>
2026-01-28 22:16:37 +0700

    create func server running command, newCLient func, unfinished command func for each other

--- server.go
+++ server.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net"
+)
+
 type server struct {
 	rooms    map[string]*room
 	commands chan command
@@ -10,4 +16,54 @@ func newServer() *server {
 		rooms:    make(map[string]*room),
 		commands: make(chan command),
 	}
+}
+
+func (s *server) run() {
+	for cmd := range s.commands {
+		switch cmd.id {
+		case CMD_NICK:
+			s.nick(cmd.client, cmd.args)
+		case CMD_JOIN:
+			s.join(cmd.client, cmd.args)
+		case CMD_ROOMS:
+			s.listRooms(cmd.client, cmd.args)
+		case CMD_MSG:
+			s.msg(cmd.client, cmd.args)
+		case CMD_QUIT:
+			s.quit(cmd.client, cmd.args)
+		}
+	}
+}
+
+func (s *server) newClient(conn net.Conn) {
+	log.Printf("new client has connected: %s", conn.RemoteAddr().String())
+
+	c := &client{
+		conn:     conn,
+		nick:     "anonymous",
+		commands: s.commands,
+	}
+
+	c.readInput()
+}
+
+func (s *server) nick(c *client, args []string) {
+	c.nick = args[1]
+	c.msg(fmt.Sprintf("Hello %s", c.nick))
+}
+
+func (s *server) join(c *client, args []string) {
+
+}
+
+func (s *server) listRooms(c *client, args []string) {
+
+}
+
+func (s *server) msg(c *client, args []string) {
+
+}
+
+func (s *server) quit(c *client, args []string) {
+
 }
